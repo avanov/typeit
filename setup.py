fd843fd27afc87173f299519ff23f1f606efe4c5
Maxim Avanov <maxim.avanov@gmail.com>
2018-07-02 14:23:16 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with (here / 'requirements.txt').open() as f:
 # ----------------------------
 
 setup(name='typeit',
-      version='0.5.1',
+      version='0.5.2',
       description='Type it!',
       long_description=README,
       classifiers=[
