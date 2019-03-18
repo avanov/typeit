b43ff51404820d31479fe100e6c84c1aacbb4988
Maxim Avanov <maxim.avanov@gmail.com>
2019-03-18 16:10:45 +0000

    Add utility for iterating over invalid data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,8 @@
 from typeit import utils
+from typing import NamedTuple, Sequence
+from typeit import type_constructor
+from enum import Enum
+import colander
 
 
 def test_normalization():
@@ -9,3 +13,32 @@ def test_normalization():
     name = 'def'
     normalized = utils.normalize_name(name)
     assert normalized == 'overridden__def'
+
+
+def test_iter_invalid_data():
+    class ItemType(Enum):
+        ONE = 'one'
+        TWO = 'two'
+
+    class Item(NamedTuple):
+        val: ItemType
+
+    class X(NamedTuple):
+        items: Sequence[Item]
+
+    mk_x, dict_x = type_constructor(X)
+
+    data = {
+        'items': [
+            {'val': 'one'},
+            {'val': 'two'},
+            {'val': 'three'},
+            {'val': 'four'},
+        ]
+    }
+
+    try:
+        x = mk_x(data)
+    except colander.Invalid as e:
+        for inv in utils.iter_invalid_data(e, data):
+            assert isinstance(inv, utils.InvalidData)
