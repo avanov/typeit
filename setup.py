9c0539b268723bb3c61c35fefb268ed0976e8b1e
Maxim Avanov <maxim.avanov@gmail.com>
2020-12-27 00:58:12 +0000

    Release 3.9.1.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with (here / 'README.rst').open() as f:
 # ----------------------------
 
 setup(name='typeit',
-      version='3.9.1.1',
+      version='3.9.1.2',
       description='typeit brings typed data into your project',
       long_description=README,
       classifiers=[
