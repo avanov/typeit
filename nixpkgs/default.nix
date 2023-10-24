2dc667ed5bf47bd8c20c0a91382d3afe61c3cf78
Maxim Avanov <601955+avanov@users.noreply.github.com>
2023-10-24 17:06:40 +0100

    Bump typing-extensions

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -3,10 +3,10 @@
 let
 
 common-src = builtins.fetchTarball {
-    name = "common-2023-03-03";
-    url = https://github.com/avanov/nix-common/archive/a30f466f3ac73842d111e80f40f287d8aa13e929.tar.gz;
+    name = "common-2023-10-24";
+    url = https://github.com/avanov/nix-common/archive/fdedd6a87d37972ca62ccfbe4f98190ed712a7bf.tar.gz;
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "sha256:1dimd334ay4jx4n81n5ms8p4i9kpyn0z7mm8xa0kcy2cpdlbq798";
+    sha256 = "sha256:1cp1fp661a8dypyqb72yacg9av93g2xpw0sjnplss6swq3rgw7cy";
 };
 
 overlays = import ./overlays.nix {};
